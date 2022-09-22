cf3f3e82fdd8efaeb813e6be4cf07a2f075111dc
奉壹想去看海 <105214475+FengYi004614@users.noreply.github.com>
2022-09-22 23:17:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -83,14 +83,14 @@ export const USER_CONFIG = {
           // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         ],
         // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-        customizedDateList: [
+//         customizedDateList: [
           // 在一起的日子
   //         {"keyword": "love_day", date: "2022-09-08"},
   //         // 结婚纪念日
   //         {"keyword": "marry_day", date: "2022-09-09"},
   //         // 退伍日
   //         {"keyword": "ex_day", date: "2022-09-10"},
-        ]
+//         ]
       }
     
     /**
